2c0e609617cc8d9f1c8acb871b71cfd407bf0f17
Hitesh Durgani <hitku.dj@gmail.com>
2026-02-16 12:40:49 +0530

    Update productOperands.py

--- productOperands.py
+++ productOperands.py
@@ -3,35 +3,37 @@
 product=15431
 a=0
 b=0
-newA=0
-newB=1
+x=0
+y=1
 extra=0
+x=product
+while x>y	:
+	if a%2==1:
+		extra+=b
+	a=int(x)
+	b=y
+	x/=2
+	y*=2
+if y>a:
+	if a%2==1:
+		extra+=b
+	a=int(x)
+	b=y
+else:
+	temp=a
+	a=b
+	b=temp					
+if extra>0 &&b%2==0:
+	b-=1
+	extra=extra+a
+	while extra-b>=0:
+		extra-=b
+		a+=1
+while extra>0&&b>a:
+	b-=2
+	extra=extra+a+a
+	while extra-b>=0:
+		extra-=b
+		a+=1
 
-newA=product
-while newA>newB:
- if a%2==1:
-  extra+=b
- a=int(newA)
- b=newB
- newA/=2
- newB*=2
-if newB<a:
- if a%2==1: 
-  extra+=b
- a=newB
- b=int(newA)
-
-n=1
-diff=a-b
-while extra>0:
- b-=1
- a+=1
- extra=extra+diff+n
- while extra-b>=0:
-  extra-=b 
-  a+=1
-  diff+=1
- n+=2
-
-print("Multiplier =", x, "Multiplicand =", y)
-
+print("HITKU:: Multiplier =", a, "Multiplicand =", b)
