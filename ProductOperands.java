fd02b5278b9f9d3fcaf6b7c80b9a9bf1b9074b9d
Hitesh Durgani <hitku.dj@gmail.com>
2026-02-16 12:21:13 +0530

    Update ProductOperands.java

--- ProductOperands.java
+++ ProductOperands.java
@@ -1,36 +1,43 @@
 public class ProductOperands{
 	public static void main(String hitku[]){
-		double product=36858443635249d,a=0,b=1,newA=0,newB=1,extra=0;
-		newA=product;
-		while(newA>newB){
+		double product=339347,a=0,b=1,x=0,y=1,extra=0;
+		x=product;
+		while(x>y){			
 			if(a%2==1){
 				extra+=b;
 			}
-			a=Math.floor(newA);
-			b=newB;	
-			newA/=2;	
-			newB*=2;			
+			a=Math.floor(x);
+			b=y;	
+			x/=2;	
+			y*=2;	
 		}
-		if(newB<a){
+		if(y>a){
 			if(a%2==1){
 				extra+=b;
 			}
-			a=newB;
-			b=Math.floor(newA);
-		}	
-		int n=1; 
-		int diff=(int) (a-b);		
-		while(extra>0){
+			a=Math.floor(x);
+			b=y;
+		}else{
+			double temp =a;
+			a=b;
+			b=temp;
+		}					
+		if(extra>0 &&b%2==0){
 			b--;
-			a++;
-			extra=extra+diff+n;
+			extra=extra+a;
 			while(extra-b>=0){
 				extra-=b;
 				a++;
-				diff++;		
 			}
-			n+=2;
 		}
-		System.out.println("Multiplier ="+a+" : Multiplicand ="+b);
+		while(extra>0&&b>a){
+			b-=2;
+			extra=extra+a+a;
+			while(extra-b>=0){
+				extra-=b;
+				a++;
+			}
+		}		
+		System.out.println("\nHITKU: "+product+" = "+a+" * "+b);		
 	}	
 }
