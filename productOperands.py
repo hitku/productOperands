6309100a4405f410f019fb72709af23b27255d80
Hitesh Durgani <hitku.dj@gmail.com>
2026-06-01 22:15:44 +0530

    Update productOperands.py

--- productOperands.py
+++ productOperands.py
@@ -1,39 +1,20 @@
 #productOperands
-
-product=15431
-a=0
-b=0
-x=0
-y=1
-extra=0
-x=product
-while x>y	:
-	if a%2==1:
-		extra+=b
-	a=int(x)
-	b=y
-	x/=2
-	y*=2
-if y>a:
-	if a%2==1:
-		extra+=b
-	a=int(x)
-	b=y
-else:
-	temp=a
-	a=b
-	b=temp					
-if extra>0 &&b%2==0:
-	b-=1
-	extra=extra+a
-	while extra-b>=0:
-		extra-=b
-		a+=1
-while extra>0&&b>a:
-	b-=2
-	extra=extra+a+a
-	while extra-b>=0:
-		extra-=b
-		a+=1
-
-print("HITKU:: Multiplier =", a, "Multiplicand =", b)
+hit=0
+ku=0
+d=319
+j=d
+hitO=1
+kuO=1
+while j>0:
+ hit+=1
+ j-=hitO
+ hitO+=2
+while j<0:
+ ku+=1
+ j+=kuO
+ kuO+=2
+ while j>0:
+  hit+=1
+  j-=hitO
+  hitO+=2
+print("HitKu:: Product =", d, " Multiplier =", hit+ku, " Multiplicand =", hit-ku)
