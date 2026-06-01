9893fe5509b1a920c2627248f94e4654a1282ed8
Hitesh Durgani <hitku.dj@gmail.com>
2026-06-01 22:12:47 +0530

    Update ProductOperands.java

--- ProductOperands.java
+++ ProductOperands.java
@@ -1,43 +1,22 @@
-public class ProductOperands{
-	public static void main(String hitku[]){
-		double product=339347,a=0,b=1,x=0,y=1,extra=0;
-		x=product;
-		while(x>y){			
-			if(a%2==1){
-				extra+=b;
-			}
-			a=Math.floor(x);
-			b=y;	
-			x/=2;	
-			y*=2;	
-		}
-		if(y>a){
-			if(a%2==1){
-				extra+=b;
-			}
-			a=Math.floor(x);
-			b=y;
-		}else{
-			double temp =a;
-			a=b;
-			b=temp;
-		}					
-		if(extra>0 &&b%2==0){
-			b--;
-			extra=extra+a;
-			while(extra-b>=0){
-				extra-=b;
-				a++;
-			}
-		}
-		while(extra>0&&b>a){
-			b-=2;
-			extra=extra+a+a;
-			while(extra-b>=0){
-				extra-=b;
-				a++;
-			}
-		}		
-		System.out.println("\nHITKU: "+product+" = "+a+" * "+b);		
-	}	
+public class productOperands{
+public static void main(String hitku[]){
+double hit=0,ku=0,d,j=529,hitO=1,kuO=1;
+d=j;
+while(j>0){
+hit++;
+j-=hitO;
+hitO+=2;
+}
+while(j<0){	
+ku++;
+j+=kuO;
+kuO+=2;
+while(j>0){
+hit++;
+j-=hitO;
+hitO+=2;
+}
+}
+System.out.println("HitKu:  "+d+" = "+(hit+ku)+" x "+(hit-ku));	
+}	
 }
