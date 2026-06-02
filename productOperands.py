56b254a1ce68884e1732ee25a345c43a851877af
Hitesh Durgani <hitku.dj@gmail.com>
2026-06-02 21:53:29 +0530

    Update productOperands.py

--- productOperands.py
+++ productOperands.py
@@ -1,10 +1,12 @@
 #productOperands
 hit=0
 ku=0
-d=319
+d=24
 j=d
 hitO=1
 kuO=1
+if d%2==0 and not d%8==0:
+ j*=2
 while j>0:
  hit+=1
  j-=hitO
@@ -17,4 +19,7 @@ while j<0:
   hit+=1
   j-=hitO
   hitO+=2
-print("HitKu:: Product =", d, " Multiplier =", hit+ku, " Multiplicand =", hit-ku)
+if d%2==0 and not d%8==0:
+ print("HitKu:: Product =", d, " Multiplier =", (hit+ku)/2, " Multiplicand =", hit-ku)
+else:
+ print("HitKu:: Product =", d, " Multiplier =", hit+ku, " Multiplicand =", hit-ku)
