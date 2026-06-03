635487f619c7f7ad751f46860102c4b5922896bc
Hitesh Durgani <hitku.dj@gmail.com>
2026-06-03 18:07:29 +0530

    Update ProductOperands.java

--- ProductOperands.java
+++ ProductOperands.java
@@ -1,26 +1,31 @@
 public class productOperands{
  public static void main(String hitku[]){
-  double hit=0,ku=0,d,j=529,hitO=1,kuO=1;
+  double hit=1,ku=1,d,j=529,h=0,k=0;
   d=j;
   if(d%2==0 && d%8!=0){
     j*=2;
   }
   while(j!=0){
    while(j>0){
-    hit++;
-    j-=hitO;
-    hitO+=2;
+    j-=hit;
+    hit+=2;
    }
    while(j<0){	
-    ku++;
-    j+=kuO;
-    kuO+=2;
+    j+=ku;
+    ku+=2;
    }
   }
-  if(d%2==0 && d%8!=0){
-   System.out.println("HitKu:  "+d+" = "+((hit+ku)/2)+" x "+(hit-ku));
-  }else{
-   System.out.println("HitKu:  "+d+" = "+(hit+ku)+" x "+(hit-ku));	
+  h=hit+ku-2;
+  k=hit-ku;
+  if(h>0){
+   h/=2;
+   if(d%2==0 && d%8!=0){
+       h/=2;
+   }
   }
+  if(k>0){
+   k/=2;
+  }
+   System.out.println("HitKu:  "+d+" = "+h+" x "+k);
  }	
 }
